0f61f3fdbf10a43de3259208748f8c410415b988
Jacob Pope <Jacob_Pope4@baylor.edu>
2023-05-09 18:36:54 -0500

    Setting up first model

--- FeedApp/models.py
+++ FeedApp/models.py
@@ -4,4 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
+class Profile(models.Model):
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=300, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
